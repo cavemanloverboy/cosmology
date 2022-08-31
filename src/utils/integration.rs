55a44556ecc5666280911a759065473b1c6cfebc
cavemanloverboy <caveycool@gmail.com>
2022-08-31 05:25:39 +0000

    add rk4 integrator

--- src/utils/integration.rs
+++ src/utils/integration.rs
@@ -2,6 +2,51 @@
 
 use quadrature::Output;
 
+use crate::scale_factor::rk4;
+
+
+pub(crate) fn rk4_integrator(
+    integrand: &dyn Fn(f64) -> f64,
+    mut lower_bound: f64,
+    upper_bound: f64,
+    mut step: f64,
+) -> f64 {
+
+    let mut result = 0.0;
+    let rk4_integrand = |x: f64, _: f64| integrand(x);
+    while lower_bound < upper_bound {
+        step = step.min(upper_bound-lower_bound);
+        result = rk4(rk4_integrand, lower_bound, result, step, None);
+        lower_bound += step;
+    }
+    result
+}
+
+// pub(crate) fn rk4_integrator_adaptive(
+//     integrand: &dyn Fn(f64) -> f64,
+//     mut lower_bound: f64,
+//     upper_bound: f64,
+//     step_hint: f64,
+//     adaptive_tol: f64,
+// ) -> f64 {
+
+//     let mut result = 0.0;
+//     let rk4_integrand = |x: f64, _: f64| integrand(x);
+//     let mut current_step; = step_hint;
+//     while lower_bound < upper_bound {
+
+//         // 
+//         current_step = step_hint.max(current_step).min(upper_bound-lower_bound);
+//         let mut temp = rk4(rk4_integrand, lower_bound, result, current_step, None);
+//         let temp2 = 
+//         while (1.0 - temp/rk4(rk4_integrand, lower_bound, result, step/2, None)).abs()  {
+//             step
+//         }
+//         result = rk4(rk4_integrand, lower_bound, result, step, None);
+//         lower_bound += step;
+//     }
+//     result
+// }
 
 pub(crate) fn two_dimensional_integral(
     integrand: &dyn Fn(f64, f64) -> f64,
